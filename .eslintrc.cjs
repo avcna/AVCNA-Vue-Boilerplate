cd751ba57eb3d1b281ca2d15b8d17f6536a94138
avcna <salsabiladitaprasetya@gmail.com>
2025-03-17 11:27:32 +0700

    add boilerplate

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen">
+    <p class="text-pink-300 font-bold text-[32px] flex justify-center">
+      Welcome to AVCNA's Vue Boilerplate
+    </p>
+  </div>
+</template>
